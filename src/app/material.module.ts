0d095773f51c12acec0b0dd479c0d5c25c654965
Kristoffer Jensen <kje@computas.com>
2018-02-21 08:24:05 +0100

    Navigation and links

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,41 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
-import {FormsModule} from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatToolbarModule],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatToolbarModule]
 })
 export class MaterialModule {}
