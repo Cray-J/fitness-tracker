de53a1fa2ff2dd0882ac0a1edce9adf293079d59
Kristoffer Jensen <kje@computas.com>
2018-02-28 16:13:39 +0100

    Using a service for current training

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,6 +1,9 @@
 import {Exercise} from './exercise.model';
+import { Subject } from 'rxjs/Subject';
 
 export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -9,6 +12,7 @@ export class TrainingService {
   ];
 
   private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
 
   getAvailableExercises() {
     return this.availableExercises.slice();
@@ -16,6 +20,33 @@ export class TrainingService {
 
   startExercise(selectedId: string) {
     this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+    this.exerciseChanged.next({ ...this.runningExercise });
+  }
+
+  completeExercise() {
+    this.exercises.push({
+      ...this.runningExercise,
+      date: new Date(),
+      state: 'completed'
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  cancelExercise(progress: number) {
+    this.exercises.push({
+      ...this.runningExercise,
+      duration: this.runningExercise.duration * (progress / 100 ),
+      calories: this.runningExercise.duration * (progress / 100 ),
+      date: new Date(),
+      state: 'cancelled'
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  getRunningExercise() {
+    return {...this.runningExercise};
   }
 
 }
