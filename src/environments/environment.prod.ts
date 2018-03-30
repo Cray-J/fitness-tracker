de03bd2413f850d2af9ed41522b72fe78fff35f7
Kristoffer Jensen <kje@computas.com>
2018-03-30 14:40:58 +0200

    Subroutes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDeOylsWJYkai8wjo1uLUZuezu1lvjWqYw',
+    authDomain: 'ng-fitness-tracker-1fb6c.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-1fb6c.firebaseio.com',
+    projectId: 'ng-fitness-tracker-1fb6c',
+    storageBucket: 'ng-fitness-tracker-1fb6c.appspot.com',
+    messagingSenderId: '270597293162'
+  }
 };
